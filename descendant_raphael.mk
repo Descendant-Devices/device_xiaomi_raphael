e0cd8e784a741ccc03273d41bd4462688cdb2874
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-02-05 10:14:15 +0100

    Descendant: bring-up and remove un-needed stuff

--- descendant_raphael.mk
+++ descendant_raphael.mk
@@ -5,21 +5,20 @@
 #
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
+$(call inherit-product, device/xiaomi/raphael/GApps-flags.mk)
 
 # Inherit some common Evolution X stuff.
-CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/descendant/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_raphael
+PRODUCT_NAME := descendant_raphael
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
